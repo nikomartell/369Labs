d5509bd83134330248f9fca780fa51edf0acb1a4
abriltr <abriltorres@arizona.edu>
2024-09-04 14:28:57 -0700

    solace

---
+++ Project 1.2/Project 1.2.srcs/sources_1/imports/Documents/1-InstructionFetchUnit/InstructionMemory_tb.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// ECE369A - Computer Architecture
+// Laboratory 
+// Module - InstructionMemory_tb.v
+// Description - Test the 'InstructionMemory_tb.v' module.
+////////////////////////////////////////////////////////////////////////////////
+
+module InstructionMemory_tb(); 
+
+    wire [31:0] Instruction;
+
+    reg [31:0] Address;
+
+	InstructionMemory u0(
+		.Address(Address),
+        .Instruction(Instruction)
+	);
+
+	initial begin
+	
+    #10; Address = 4;
+    #10; Address = 8;
+    #10; Address = 12;
+    #10; Address = 16;
+    
+	end
+
+endmodule
+
---
+++ Project 1.2/Project 1.2.srcs/sources_1/imports/Documents/1-InstructionFetchUnit/ProgramCounter_tb.v
@@ -0,0 +1,42 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// ECE369A - Computer Architecture
+// Laboratory 1
+// Module - ProgramCounter_tb.v
+// Description - Test the 'ProgramCounter.v' module.
+////////////////////////////////////////////////////////////////////////////////
+
+module ProgramCounter_tb(); 
+
+	reg [31:0] Address;
+	reg Reset, Clk;
+
+	wire [31:0] PCResult;
+
+    ProgramCounter u0(
+        .Address(Address), 
+        .PCResult(PCResult), 
+        .Reset(Reset), 
+        .Clk(Clk)
+    );
+
+	initial begin
+		Clk <= 1'b0;
+		forever #10 Clk <= ~Clk;
+	end
+
+	initial begin
+	
+   Address = 1;
+   Reset = 0;
+   
+   #30 Reset = 1;
+   #60 Reset = 2;
+   #90 Reset = 3;
+   #120 Reset = 4;
+	
+	end
+
+endmodule
+
