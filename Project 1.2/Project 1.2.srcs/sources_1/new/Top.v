cd371f05c303de83d93d858e8c996824ab612782
abriltr <abriltorres@arizona.edu>
2024-09-04 14:20:04 -0700

    create top.v

---
+++ Project 1.2/Project 1.2.srcs/sources_1/new/Top.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/04/2024 02:16:01 PM
+// Design Name: 
+// Module Name: Top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Top(
+    input Reset;
+    input Clk;
+    
+
+
+
+    );
+endmodule
