f3c65810212179e3616df0af4a5da15f3b1272ed
Niko Martell <niko.martell3@gmail.com>
2024-09-19 00:33:17 -0700

    C implementation of Lab 3

--- Lab 3/psuedo.c
+++ Lab 3/psuedo.c
@@ -1,32 +1,60 @@
-
+#include <stdio.h>
 
 int main(){
 
 int frame[12][12];
 int window[4][4];
 
-int fx, fy, wx, wy = 0;
-int fxs, fys, wxs, wys;
+int SADfx, SADfy = 0;
+int fx, fy, wx, wy;
 
-fxs = 12;
-fys = 12;
-wxs = 4;
-wys = 4;
-int i,j,k,l;
+
+fx = sizeof(frame[0]);
+fy = sizeof(frame[0]);
+wx = sizeof(window[0]);
+wy = sizeof(window[0]); // store/get the size of the frame and window
+
+int k,l;                // loop variables / t0 and t1
 double SAD = 99999999999;
-double temp = 0;
+double temp = 0;    
 
-for (i = wx; i < fxs; i++){
-    for (j = wy; j < fys; j++){
-        temp = 0;
-        for (k = wx; k < wxs; k++) {
-            for (l = wy; l < wys; l++) {
-                temp += abs(frame[i + k][j + l] - window[k][l]);
+int centerX = fx / 2;
+int centerY = fy / 2;
+int radius = fx / 2;    // maximum radius is half of the frame size
+int direction = 0;      // 0: right, 1: down, 2: left, 3: up
+
+while (radius <= 0) {
+    int x = centerX - radius;   // start from the top left corner of the square
+    int y = centerY - radius;   // subtract the radius to get the top left corner of the square
+
+    for (int step = 0; step < radius * 2; step++) {
+        if (x >= 0 && x < fx - wx && y >= 0 && y < fy - wy) {   // check if the window is inside the frame with x as the left most side of the window 
+            temp = 0;
+            for (k = 0; k < wx; k++) {
+                for (l = 0; l < wy; l++) {
+                    temp += abs(frame[x + k][y + l] - window[k][l]);
+                }
+            }
+            if (temp < SAD) {
+                SAD = temp;
+                SADfx = x;
+                SADfy = y;
             }
         }
-        if (temp < SAD) {
-            SAD = temp;
+
+        switch (direction) {
+            case 0: x++; break; // right
+            case 1: y++; break; // down
+            case 2: x--; break; // left
+            case 3: y--; break; // up
         }
     }
 
+    if (direction == 4) { 
+        radius--;       // if we have completed a circle, decrease the radius
+    }
+
+    direction = (direction + 1) % 4;
+    
+}
 }
