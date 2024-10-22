8d8dd3dcf4447e694048c71d8598fbf75a95f9f3
abriltr <abriltorres@arizona.edu>
2024-10-21 18:25:52 -0700

    Beyonce Controller.v

--- Main/DatapathComponents/Controller.v
+++ Main/DatapathComponents/Controller.v
@@ -23,131 +23,204 @@
 module Controller(
     input [5:0] OPCode,
     input [5:0] Function, 
-    input beq, 
-    input blt, 
-    input bgt,
+    input [4:0] TargetReg,
     
     //Control signals 
     output reg RegWrite,
     output reg [3:0] ALUOp,
-    output reg ALUSrc,
-    output reg [2:0] RegDst,
+    output reg ALUSrc, //this is a mux 1-passes imm value
+    output reg [2:0] RegDst, //this is a mux 0-passes rt, 1-passes rd 
     output reg MemWrite,
     output reg MemRead,
     output reg Branch,
-    output reg MemToReg,
+    output reg MemToReg, //this is a 2x1 mux
     output reg Jump,
-    output reg [1:0] PCSrc,
-    output reg StoreType,
-    output reg LoadType
+    output reg [1:0] StoreType, //this is a 3x1 mux
+    output reg [1:0] LoadType,  //this is a 3x1 mux
+    output reg JumpReg
     ); 
     
     // Initialize outputs to default values
-        initial begin 
+    initial begin 
     RegWrite <= 0;
     ALUSrc <= 0;
     RegDst <= 0;
     MemWrite <= 0;
     MemRead <= 0;
     MemToReg <= 0;
-    PCSrc <= 0;
     Branch <= 0;
     Jump <= 0;
     LoadType <= 0;
     StoreType <= 0;
     ALUOp <= 0;
-        end 
+    JumpReg <= 0; 
+    end 
     
     always@(OPCode) begin 
+    //default values again to prevent latch inference 
+    RegWrite <= 0;
+    ALUSrc <= 0;
+    RegDst <= 0;
+    MemWrite <= 0;
+    MemRead <= 0;
+    MemToReg <= 0;
+    Branch <= 0;
+    Jump <= 0;
+    LoadType <= 0;
+    StoreType <= 0;
+    ALUOp <= 0;
+    JumpReg <= 0;
+    
     case (OPCode)
             //R-type, JR, SLL, SRL 
             6'b000000: begin 
-                case(Function)
+            case(Function)
             //JR 
              6'b001000: begin
-                ALUOp <= 2;
+                ALUOp <=  6'b000000;
                 RegWrite <= 0; 
                 RegDst <= 0;
                 ALUSrc <= 0;
                 MemWrite <= 0; 
                 MemRead <= 0;
                 MemToReg <= 0;
-                PCSrc <= 0;
-                Branch <= 0;
-                Jump <= 1;
-                LoadType <= 0;
-                StoreType <= 0; 
-             end 
-            /*
-                RegWrite <= 1; 
-                RegDst <= 1;
-                ALUSrc <= 0;
-                MemWrite <= 0; 
-                MemRead <= 0;
-                MemToReg <= 1;
-                PCSrc <= 0;
-                Branch <= 0;
-                Jump <= 0; 
-            end */
-            // MUL
-             6'b011100: begin 
-                ALUOp <= 0;
-                RegWrite <= 1; 
-                RegDst <= 1;
-                ALUSrc <= 0;
-                MemWrite <= 0; 
-                MemRead <= 0;
-                MemToReg <= 1;
-                PCSrc <= 0;
                 Branch <= 0;
                 Jump <= 0;
                 LoadType <= 0;
                 StoreType <= 0; 
+                JumpReg <= 1;
+             end 
+            //R-type: and, add, sub, or, nor, xor, sll, slt, srl 
+            default: begin
+            //ALUOp is func 
+                ALUOp <= 6'b000000; 
+                RegWrite <= 1; 
+                RegDst <= 1;
+                ALUSrc <= 0;
+                MemWrite <= 0; 
+                MemRead <= 0;
+                MemToReg <= 0;
+                Branch <= 0;
+                Jump <= 0;
+                LoadType <= 0;
+                StoreType <= 0; 
+                JumpReg <= 0;
+            end 
+            endcase 
+            end 
+            
+            // MUL
+             6'b011100: begin 
+                ALUOp <=  6'b011100;
+                RegWrite <= 1; 
+                RegDst <= 1;
+                ALUSrc <= 0;
+                MemWrite <= 0; 
+                MemRead <= 0;
+                MemToReg <= 1;
+                Branch <= 0;
+                Jump <= 0;
+                LoadType <= 0;
+                StoreType <= 0; 
+                JumpReg <= 0;
             end
+            
+            //Immediate
             //ANDI
             6'b001100: begin 
+                ALUOp <= 6'b001100;  
                 RegWrite <= 1; 
                 RegDst <= 0;
                 ALUSrc <= 1;
                 MemWrite <= 0; 
                 MemRead <= 0;
                 MemToReg <= 1;
-                PCSrc <= 0;
                 Branch <= 0;
                 Jump <= 0;
                 LoadType <= 0;
                 StoreType <= 0; 
+                JumpReg <= 0;
             end
             // ADDI
            6'b001000: begin  
+                ALUOp <= 6'b001000;
                 RegWrite <= 1; 
                 RegDst <= 0;
                 ALUSrc <= 1;
                 MemWrite <= 0; 
                 MemRead <= 0;
                 MemToReg <= 1;
-                PCSrc <= 0;
                 Branch <= 0;
                 Jump <= 0;
                 LoadType <= 0;
                 StoreType <= 0;
+                JumpReg <= 0;
+
             end
+            //ORI
+             6'b001101: begin 
+                ALUOp <= 6'b001101;
+                RegWrite <= 1; 
+                RegDst <= 0;
+                ALUSrc <= 1;
+                MemWrite <= 0; 
+                MemRead <= 0;
+                MemToReg <= 1;
+                Branch <= 0;
+                Jump <= 0;
+                LoadType <= 0;
+                StoreType <= 0;
+                JumpReg <= 0;
+                end
+             //XORI 
+             6'b001110: begin
+                ALUOp <= 6'b001110;
+                RegWrite <= 1; 
+                RegDst <= 0;
+                ALUSrc <= 1;
+                MemWrite <= 0; 
+                MemRead <= 0;
+                MemToReg <= 1;
+                Branch <= 0;
+                Jump <= 0;
+                LoadType <= 0;
+                StoreType <= 0;
+                JumpReg <= 0;
+                end
+              //SLTI
+             6'b001010: begin 
+                ALUOp <= 9;
+                RegWrite <= 1; 
+                RegDst <= 0;
+                ALUSrc <= 1;
+                MemWrite <= 0; 
+                MemRead <= 0;
+                MemToReg <= 1;
+                Branch <= 0;
+                Jump <= 0;
+                LoadType <= 0;
+                StoreType <= 0;
+                JumpReg <= 0;
+                end 
+                
+            //loads and stores    
             //LW 
             6'b100011: begin 
+                ALUOp <= 6'b100011; 
                 RegWrite <= 1; 
                 RegDst <= 0;
                 ALUSrc <= 1;
                 MemWrite <= 0; 
                 MemRead <= 1;
                 MemToReg <= 0;
-                PCSrc <= 0;
                 Branch <= 0;
                 Jump <= 0;
-                LoadType <= 1;
+                LoadType <= 0;
                 StoreType <= 0;
             end
             //SW 
             6'b101011: begin
+                ALUOp <=  6'b101011;
                 RegWrite <= 0; 
                 RegDst <= 0;
                 ALUSrc <= 1;
@@ -157,31 +230,31 @@ module Controller(
                 Branch <= 0;
                 Jump <= 0;
                 LoadType <= 0;
-                StoreType <= 1;
+                StoreType <= 0;
             end
             //SB
             6'b101000: begin
+                ALUOp <= 6'b101000;
                 RegWrite <= 0; 
                 RegDst <= 0;
                 ALUSrc <= 1;
                 MemWrite <= 1; 
                 MemRead <= 0;
                 MemToReg <= 0;
-                PCSrc <= 0;
                 Branch <= 0;
                 Jump <= 0;
                 LoadType <= 0;
-                StoreType <= 1;
+                StoreType <= 2; //2 for byte, 1 for half, 0 for whole
             end
             //LH 
             6'b100001: begin
+                ALUOp <=  6'b100001;
                 RegWrite <= 1; 
                 RegDst <= 0;
                 ALUSrc <= 1;
                 MemWrite <= 0; 
                 MemRead <= 1;
                 MemToReg <= 0;
-                PCSrc <= 0;
                 Branch <= 0;
                 Jump <= 0;
                 LoadType <= 1;
@@ -189,56 +262,77 @@ module Controller(
              end 
              //LB 
              6'b100000: begin
+                ALUOp <= 6'b100000;
                 RegWrite <= 1; 
                 RegDst <= 0;
                 ALUSrc <= 1;
                 MemWrite <= 0; 
                 MemRead <= 1;
                 MemToReg <= 0;
-                PCSrc <= 0;
                 Branch <= 0;
                 Jump <= 0;
-                LoadType <= 1;
+                LoadType <= 2;
                 StoreType <= 0;
              end 
              //SH 
              6'b101001: begin
+                ALUOp <= 6'b101001;
                 RegWrite <= 0; 
                 RegDst <= 0;
                 ALUSrc <= 1;
                 MemWrite <= 1; 
                 MemRead <= 0;
                 MemToReg <= 0;
-                PCSrc <= 0;
                 Branch <= 0;
                 Jump <= 0;
                 LoadType <= 0;
                 StoreType <= 1;
              end 
+             
+             //branches 
+             //bgez and bltz - solved issue with same op code
+             6'b000001: begin
+             case(TargetReg)
              //BGEZ
-             6'b000001: begin 
-             RegWrite <= 0; 
+             5'b00001: begin 
+                ALUOp <= 9;
+                RegWrite <= 0; 
                 RegDst <= 0;
                 ALUSrc <= 0;
                 MemWrite <= 0; 
                 MemRead <= 0;
                 MemToReg <= 0;
-                PCSrc <= 0;
                 Branch <= 1;
                 Jump <= 0;
                 LoadType <= 0;
                 StoreType <= 0;
              end 
-             //BEQ
-             6'b000100: begin
+             //BLTZ 
+             5'b00000: begin
+                ALUOp <= 11;
                 RegWrite <= 0; 
-                ALUOp <= 1; //sub 
                 RegDst <= 0;
                 ALUSrc <= 0;
                 MemWrite <= 0; 
                 MemRead <= 0;
                 MemToReg <= 0;
-                PCSrc <= 0;
+                Branch <= 1;
+                Jump <= 0;
+                LoadType <= 0;
+                StoreType <= 0;
+             end 
+             endcase
+             end
+             
+             //BEQ
+             6'b000100: begin
+                ALUOp <= 4;
+                RegWrite <= 0; 
+                RegDst <= 0;
+                ALUSrc <= 0;
+                MemWrite <= 0; 
+                MemRead <= 0;
+                MemToReg <= 0;
                 Branch <= 1;
                 Jump <= 0;
                 LoadType <= 0;
@@ -246,13 +340,13 @@ module Controller(
              end
              //BNE 
              6'b000101: begin
-             RegWrite <= 0; 
+                ALUOp <= 5;
+                RegWrite <= 0; 
                 RegDst <= 0;
                 ALUSrc <= 0;
                 MemWrite <= 0; 
                 MemRead <= 0;
                 MemToReg <= 0;
-                PCSrc <= 0;
                 Branch <= 1;
                 Jump <= 0;
                 LoadType <= 0;
@@ -260,13 +354,13 @@ module Controller(
              end 
              //BGTZ
              6'b000001: begin
-             RegWrite <= 0; 
+                ALUOp <= 7;
+                RegWrite <= 0; 
                 RegDst <= 0;
                 ALUSrc <= 0;
                 MemWrite <= 0; 
                 MemRead <= 0;
                 MemToReg <= 0;
-                PCSrc <= 0;
                 Branch <= 1;
                 Jump <= 0;
                 LoadType <= 0;
@@ -274,41 +368,29 @@ module Controller(
              end 
              //BLEZ
              6'b000110: begin
-             RegWrite <= 0; 
-                RegDst <= 0;
-                ALUSrc <= 0;
-                MemWrite <= 0; 
-                MemRead <= 0;
-                MemToReg <= 0;
-                PCSrc <= 0;
-                Branch <= 1;
-                Jump <= 0;
-                LoadType <= 0;
-                StoreType <= 0;
-             end 
-             //BLTZ - same Op code as bgez - need to solve issue 
-             6'b000001: begin
-             RegWrite <= 0; 
-                RegDst <= 0;
-                ALUSrc <= 0;
-                MemWrite <= 0; 
-                MemRead <= 0;
-                MemToReg <= 0;
-                PCSrc <= 0;
-                Branch <= 1;
-                Jump <= 0;
-                LoadType <= 0;
-                StoreType <= 0;
-             end 
-             //J 
-             6'b000010: begin
+                ALUOp <= 13;
                 RegWrite <= 0; 
                 RegDst <= 0;
                 ALUSrc <= 0;
                 MemWrite <= 0; 
                 MemRead <= 0;
                 MemToReg <= 0;
-                PCSrc <= 0;
+                Branch <= 1;
+                Jump <= 0;
+                LoadType <= 0;
+                StoreType <= 0;
+             end 
+             
+             //jumps 
+             //J 
+             6'b000010: begin
+                //ALUOp <= ?; we do not have jump in alu control
+                RegWrite <= 0; 
+                RegDst <= 0;
+                ALUSrc <= 0;
+                MemWrite <= 0; 
+                MemRead <= 0;
+                MemToReg <= 0;
                 Branch <= 0;
                 Jump <= 1;
                 LoadType <= 0;
@@ -317,146 +399,20 @@ module Controller(
              
              //JAL 
              6'b000011: begin
-                RegWrite <= 0; 
-                RegDst <= 0;
+                ALUOp <= 0; //arbitrary value
+                RegWrite <= 1; 
+                RegDst <= 2;
                 ALUSrc <= 0;
                 MemWrite <= 0; 
-                MemRead <= 0;
-                MemToReg <= 0;
-                PCSrc <= 0;
+                MemRead <= 1;
+                MemToReg <= 2;
                 Branch <= 0;
                 Jump <= 1;
                 LoadType <= 0;
                 StoreType <= 0;
              end 
-             //OR 
-             6'b100101: begin
-                RegWrite <= 1; 
-                RegDst <= 0;
-                ALUSrc <= 0;
-                MemWrite <= 0; 
-                MemRead <= 0;
-                MemToReg <= 1;
-                PCSrc <= 0;
-                Branch <= 0;
-                Jump <= 0;
-                LoadType <= 0;
-                StoreType <= 0;
-             end 
-             //NOR 
-             6'b100111: begin
-                RegWrite <= 1; 
-                RegDst <= 0;
-                ALUSrc <= 0;
-                MemWrite <= 0; 
-                MemRead <= 0;
-                MemToReg <= 1;
-                PCSrc <= 0;
-                Branch <= 0;
-                Jump <= 0;
-                LoadType <= 0;
-                StoreType <= 0;
-             end 
-             //XOR 
-             6'b100110: begin
-                RegWrite <= 1; 
-                RegDst <= 0;
-                ALUSrc <= 0;
-                MemWrite <= 0; 
-                MemRead <= 0;
-                MemToReg <= 1;
-                PCSrc <= 0;
-                Branch <= 0;
-                Jump <= 0;
-                LoadType <= 0;
-                StoreType <= 0;
-             end 
-         
-             //ORI
-             6'b001101: begin 
-                RegWrite <= 1; 
-                RegDst <= 0;
-                ALUSrc <= 1;
-                MemWrite <= 0; 
-                MemRead <= 0;
-                MemToReg <= 1;
-                PCSrc <= 0;
-                Branch <= 0;
-                Jump <= 0;
-                LoadType <= 0;
-                StoreType <= 0;
-                end
-             //XORI 
-             6'b001110: begin
-                RegWrite <= 1; 
-                RegDst <= 0;
-                ALUSrc <= 1;
-                MemWrite <= 0; 
-                MemRead <= 0;
-                MemToReg <= 1;
-                PCSrc <= 0;
-                Branch <= 0;
-                Jump <= 0;
-                LoadType <= 0;
-                StoreType <= 0;
-                end
-             //SLL
-             6'b000000: begin 
-                RegWrite <= 1; 
-                RegDst <= 1;
-                ALUSrc <= 0;
-                MemWrite <= 0; 
-                MemRead <= 0;
-                MemToReg <= 1;
-                PCSrc <= 0;
-                Branch <= 0;
-                Jump <= 0;
-                LoadType <= 0;
-                StoreType <= 0;
-                end
-             //SRL
-             6'b000010: begin 
-                RegWrite <= 1; 
-                RegDst <= 1;
-                ALUSrc <= 0;
-                MemWrite <= 0; 
-                MemRead <= 0;
-                MemToReg <= 1;
-                PCSrc <= 0;
-                Branch <= 0;
-                Jump <= 0;
-                LoadType <= 0;
-                StoreType <= 0;
-                end
-             //SLT
-             6'b101010: begin 
-                RegWrite <= 1; 
-                RegDst <= 1;
-                ALUSrc <= 0;
-                MemWrite <= 0; 
-                MemRead <= 0;
-                MemToReg <= 1;
-                PCSrc <= 0;
-                Branch <= 0;
-                Jump <= 0;
-                LoadType <= 0;
-                StoreType <= 0;
-                end
-             //SLTI
-             6'b001010: begin 
-                RegWrite <= 1; 
-                RegDst <= 0;
-                ALUSrc <= 1;
-                MemWrite <= 0; 
-                MemRead <= 0;
-                MemToReg <= 1;
-                PCSrc <= 0;
-                Branch <= 0;
-                Jump <= 0;
-                LoadType <= 0;
-                StoreType <= 0;
-                end 
-                
+          
+            
         endcase     
       end 
  
