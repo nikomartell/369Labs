5851e78d16e4c64ee656bd2ad7bf3df8c5ff5539
Maress0 <maress@arizona.edu>
2024-10-22 11:36:45 -0700

    Update DecodeUnit

--- Main/DatapathComponents/DecodeUnit.v
+++ Main/DatapathComponents/DecodeUnit.v
@@ -1,5 +1,7 @@
 module DecodeUnit (
-    input [31:0] ReadData,
+    input [31:0] MemOut,
+    input [31:0] Rt,
+    input [5:0] opcode,
     input [1:0] DecodeOp, // Assuming DecodeOp is a 2-bit control signal
     output reg [31:0] decode_result
 );
@@ -9,24 +11,33 @@ always @(*) begin
     // Still need the seperate values for opcode and base
 
     // Set the decode result based on the opcode and base
-    case (opcode)
-        7'b1100011: // Branch instructions
-            decode_result = base;
-        default:
-            decode_result = 32'b0; // Default case
-    endcase
-
-    // Additional logic based on DecodeOp
-    case (DecodeOp)
-        2'b00: // Load half byte
-            // Add your logic for load half byte here
-        2'b01: // Load byte
-            // Add your logic for load byte here
-        2'b10: // Store half byte
-            // Add your logic for store half byte here
-        default:
-            // Default case for DecodeOp
-    endcase
+    if (opcode == 6'b100011 || opcode == 6'b101011) begin //LW or SW
+        decode_result <= MemOut;
+    end
+    else if (opcode == 6'b100001 || opcode == 6'b101001) begin //LH or SH
+        if (DecodeOp[1]) begin 
+            decode_result <= {Rt[31:16], MemOut[15:0]};
+        end
+        else begin 
+            decode_result <= {MemOut[31:16], Rt[15:0]};
+        end
+    end
+    else if (opcode == 6'b100000 || opcode == 6'b101000) begin //LB or SB
+        case (DecodeOp) 
+            2'b00: begin
+                decode_result <= {MemOut[31:8], Rt[7:0]};
+            end
+            2'b01: begin
+                decode_result <= {MemOut[31:16], Rt[15:8], MemOut[7:0]};
+            end
+            2'b10: begin
+                decode_result <= {MemOut[31:24], Rt[23:16], MemOut[15:0]};
+            end
+            2'b11: begin 
+                decode_result <= {Rt[31:24], MemOut[23:0]};
+            end
+        endcase
+    end
 end
 
 endmodule
