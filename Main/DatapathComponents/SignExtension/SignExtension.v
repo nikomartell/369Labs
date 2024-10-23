4543152d448b30b8b52411d6a69a453b0c476b32
abriltr <abriltorres@arizona.edu>
2024-10-22 18:31:51 -0700

    Update SignExtension.v

--- Main/DatapathComponents/SignExtension/SignExtension.v
+++ Main/DatapathComponents/SignExtension/SignExtension.v
@@ -11,15 +11,7 @@ module SignExtension(in, out);
     input [15:0] in;
     
     /* A 32-Bit output word */
-    output reg [31:0] out;
+    output [31:0] out;
     
-    always @(in) begin 
-        if (in[15]) begin 
-            out <= ~(in) + 1;
-        end
-        else begin
-            out <= {16'b0, in};
-        end
-    end
-
+     assign out = { {16{in[15]}}, in }; // Concatenatation with input and extended input
 endmodule
