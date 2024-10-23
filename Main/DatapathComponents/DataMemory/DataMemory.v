989007972eb8fff356a3a9eba1fc2d7517f7179e
abriltr <abriltorres@arizona.edu>
2024-10-22 18:30:26 -0700

    Update DataMemory.v

--- Main/DatapathComponents/DataMemory/DataMemory.v
+++ Main/DatapathComponents/DataMemory/DataMemory.v
@@ -18,49 +18,48 @@
 //
 // FUNCTIONALITY:-
 // Design the above memory similar to the 'RegisterFile' model in the previous 
-// assignment.  Create a 1K memory, for which we need 10 bits.  In order to 
+// assignment. Create a 1K memory, for which we need 10 bits. In order to 
 // implement byte addressing, we will use bits Address[11:2] to index the 
 // memory location. The 'WriteData' value is written into the address 
 // corresponding to Address[11:2] in the positive clock edge if 'MemWrite' 
 // signal is 1. 'ReadData' is the value of memory location Address[11:2] if 
 // 'MemRead' is 1, otherwise, it is 0x00000000. The reading of memory is not 
 // clocked.
-//
-// you need to declare a 2d array. in this case we need an array of 1024 (1K)  
-// 32-bit elements for the memory.   
-// for example,  to declare an array of 256 32-bit elements, declaration is: reg[31:0] memory[0:255]
-// if i continue with the same declaration, we need 8 bits to index to one of 256 elements. 
-// however , address port for the data memory is 32 bits. from those 32 bits, least significant 2 
-// bits help us index to one of the 4 bytes within a single word. therefore we only need bits [9-2] 
-// of the "Address" input to index any of the 256 words. 
 ////////////////////////////////////////////////////////////////////////////////
 
-module DataMemory(Address, WriteData, Clk, MemWrite, MemRead, ReadData); 
+module DataMemory(
+    input [31:0] Address,    // Input Address 
+    input [31:0] WriteData,  // Data that needs to be written into the address 
+    input Clk,
+    input MemWrite,          // Control signal for memory write 
+    input MemRead,           // Control signal for memory read 
+    output reg [31:0] ReadData // Contents of memory location at Address
+); 
 
-    input [31:0] Address; 	// Input Address 
-    input [31:0] WriteData; // Data that needs to be written into the address 
-    input Clk;
-    input MemWrite; 		// Control signal for memory write 
-    input MemRead; 			// Control signal for memory read 
-
-    output reg[31:0] ReadData; // Contents of memory location at Address
-
-    // Declare the memory array
+    // Declare the memory array 1024 - 32-bit words
     reg [31:0] memory [0:1023];
 
+    // Initialize memory to zero
+    integer i;
+    initial begin
+        for (i = 0; i < 1024; i = i + 1) begin
+            memory[i] = 32'b0; // Set each memory location to zero
+        end
+    end
+    
     always @(posedge Clk) begin
         // Write operation
         if (MemWrite) begin
-            memory[Address[11:2]] <= WriteData;
+            memory[Address[11:2]] <= WriteData; // Use Address[11:2] for word alignment
         end
     end
 
     always @(*) begin
         // Read operation
         if (MemRead) begin
-            ReadData = memory[Address[11:2]];
+            ReadData = memory[Address[11:2]]; // Read data if MemRead is asserted
         end else begin
-            ReadData = 32'b0;
+            ReadData = 32'b0; // Return 0 if MemRead is not asserted
         end
     end
 
