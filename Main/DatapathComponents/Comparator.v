98401cddd58ac6b0a6d55283b371cae11c1dc98b
abriltr <abriltorres@arizona.edu>
2024-10-22 18:33:57 -0700

    Update Comparator.v

--- Main/DatapathComponents/Comparator.v
+++ Main/DatapathComponents/Comparator.v
@@ -20,26 +20,39 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module Comparator(Reg1, Reg2, beq, blt, bgt);
+module Comparator(Reg1, Reg2, beq, blt, bgt, zero);
     input [31:0] Reg1;
     input [31:0] Reg2;
-    output reg beq, blt, bgt;
+    output reg beq, blt, bgt, zero;
     
-    always @(Reg1, Reg2) begin
-        if (Reg1 > Reg2) begin
-            beq <= 0; 
-            blt <= 0; 
-            bgt <= 1; 
+    always @(Reg1, Reg2) begin 
+        if (Reg1 > Reg2) begin 
+            beq <= 0;
+            blt <= 0;
+            bgt <= 1;
+            zero <= 0;
         end
-        else if (Reg1 < Reg2) begin
+        else if (Reg1 < Reg2) begin 
             beq <= 0;
             blt <= 1;
             bgt <= 0;
+            zero <= 0;
         end
         else if (Reg1 == Reg2) begin 
             beq <= 1;
             blt <= 0;
             bgt <= 0;
-        end      
+            zero <= 0;
+        end
+        else if (Reg1 == 0) begin 
+            beq <= 0;
+            blt <= 0;
+            bgt <= 0;
+            zero <= 1;
+        end
+        
+        
     end
+    
+    
 endmodule
