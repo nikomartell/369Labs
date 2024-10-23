caec88c0c987e04f1542e1a21f09e88e04fd47a7
abriltr <abriltorres@arizona.edu>
2024-10-22 22:59:15 -0700

    Update mem_wb.v

--- Main/DatapathComponents/mem_wb.v
+++ Main/DatapathComponents/mem_wb.v
@@ -1,31 +1,48 @@
+`timescale 1ns / 1ps
 module mem_wb (
     input wire clk,
     input wire reset,
+    
     input wire [31:0] ALUResult,
     input wire [31:0] mem_read,
-    input wire [4:0] RegDst,
-    input wire MemtoReg,
-    input wire RegWrite,
-    output reg [31:0] alu_out_wb,
+    input wire [4:0] regdst, //write register in 
+    input wire [31:0] pc_in, 
+    
+    output reg [31:0] ALUResult_out,
     output reg [31:0] mem_read_out, 
-    output reg [4:0] RegDst_out,
-    output reg MemtoReg_out,
-    output reg RegWrite_out
+    output reg [4:0] regdst_out, //write register out
+    output reg [31:0] pc_out,
+    
+    //input control signals 
+    input wire [1:0] memtoreg,
+    input wire regwrite,
+    
+    //output control signals 
+    output reg [1:0] memtoreg_out,
+    output reg regwrite_out
+    
 );
 
 always @(posedge clk or posedge reset) begin
     if (reset) begin
-        alu_out_wb <= 32'b0;
+        ALUResult_out <= 32'b0;
         mem_read_out <= 32'b0;
-        RegDst_out <= 5'b0;
-        RegWrite_out <= 1'b0;
-        MemtoReg_out <= 1'b0;
+        regdst_out <= 5'b0;
+        pc_out <= 32'b0;
+        
+        //control signals reset 
+        regwrite_out <= 1'b0;
+        memtoreg_out <= 1'b0;
+        
     end else begin
-        alu_out_wb <= ALUResult;
+        ALUResult_out <= ALUResult;
         mem_read_out <= mem_read;
-        RegDst_out <= RegDst;
-        RegWrite_out <= RegWrite;
-        MemtoReg_out <= MemtoReg;
+        regdst_out <= regdst;
+        pc_out <= pc_in;
+        
+        //control signals passing 
+        regwrite_out <= regwrite;
+        memtoreg_out <= memtoreg;
     end
 end
 
