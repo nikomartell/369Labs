5b7364c88085974f413a84db894e50f20e911954
abriltr <abriltorres@arizona.edu>
2024-10-22 22:47:02 -0700

    Update ex_mem.v

--- Main/DatapathComponents/ex_mem.v
+++ Main/DatapathComponents/ex_mem.v
@@ -1,30 +1,37 @@
 module ex_mem (
     input clk,
     input reset,
-    //input [31:0] add_result,  not needed as branch moved to decode phase
-    //input alu_zero,    zero not needed as branch moved to decode phase
+    
+    //input values
+   // input [31:0] add_result, not needed as branch moved to decode phase
+   // input alu_zero,    zero not needed as branch moved to decode phase
+    input [31:0] pc_in, //pc adder 4 in
     input [31:0] alu_result,
-    input [31:0] read_data2,
-    input [4:0] instruction_mux, 
-    input PCSrc,
-    input memRead,
-    input memWrite,
-    input MemtoReg,
-    input RegWrite,
-    input [5:0] ALUop,
+    input [31:0] read_data2, //read data2 in 
+    input [4:0] instruction_mux, //write register in mux 
+    // input zero_signal_in, 
+    
     //output reg [31:0] add_result_out,
     //output reg alu_zero_out,
+    output reg [31:0] pc_out, //pc adder 4 out 
     output reg [31:0] alu_result_out,
     output reg [31:0] read_data2_out,
-    output reg [4:0] instruction_mux_out,
-    output reg PCSrc_out,
-    output reg memRead_out,
-    output reg memWrite_out,
-    output reg MemtoReg_out,
-    output reg RegWrite_out,
-    output reg [5:0] ALUop_out
+    output reg [4:0] instruction_mux_out, //write reg out mux 
+    
+    //input control signals 
+    input regwrite, //reg write in 
+    input memwrite, 
+    input memread, 
+    input [1:0] memtoreg,
+   
+    //output control signals 
+    output reg regwrite_out, 
+    output reg memwrite_out,
+    output reg memread_out, 
+    output reg [1:0] memtoreg_out
 );
 
+//
 always @(posedge clk or posedge reset) begin
     if (reset) begin
         //add_result_out <= 32'b0;
@@ -32,24 +39,28 @@ always @(posedge clk or posedge reset) begin
         alu_result_out <= 32'b0;
         read_data2_out <= 32'b0;
         instruction_mux_out <= 5'b0;
-        PCSrc_out <= 1'b0;
-        memRead_out <= 1'b0;
-        memWrite_out <= 1'b0;
-        MemtoReg_out <= 1'b0;
-        RegWrite_out <= 1'b0;
-        ALUop_out <= 1'b0;
+        pc_out <= 32'b0;
+        
+        //control signals reset 
+        memread_out <= 1'b0;
+        memwrite_out <= 1'b0;
+        memtoreg_out <= 1'b0;
+        regwrite_out <= 1'b0;
+        
+        
     end else begin
         //add_result_out <= add_result;
         //alu_zero_out <= alu_zero;
         alu_result_out <= alu_result;
         read_data2_out <= read_data2;
         instruction_mux_out <= instruction_mux;
-        PCSrc_out <= PCSrc;
-        memRead_out <= memRead;
-        memWrite_out <= memWrite;
-        MemtoReg_out <= MemtoReg;
-        RegWrite_out <= RegWrite;
-        ALUop_out <= ALUop;
+        pc_out <= pc_in;
+        
+        //control signals passing 
+        memread_out <= memread;
+        memwrite_out <= memwrite;
+        memtoreg_out <= memtoreg;
+        regwrite_out <= regwrite;
     end
 end
 endmodule
