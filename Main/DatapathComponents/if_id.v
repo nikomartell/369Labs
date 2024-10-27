f097c76c8a49450d15f15457f42e97d540241cc9
abriltr <abriltorres@arizona.edu>
2024-10-27 13:06:48 -0700

    Update if_id.v

--- Main/DatapathComponents/if_id.v
+++ Main/DatapathComponents/if_id.v
@@ -1,13 +1,12 @@
-`timescale 1ns / 1ps
 module if_id (
     input wire clk,
     input wire reset,
     
-    input wire [31:0] pc_in, //pc adder 4 in
-    input wire [31:0] instr_in, //instruction in
+    input wire [31:0] pc_in,
+    input wire [31:0] instr_in,
     
-    output reg [31:0] pc_out, //pc adder 4 out 
-    output reg [31:0] instr_out //instruction out
+    output reg [31:0] pc_out,
+    output reg [31:0] instr_out
 );
 
 always @(posedge clk or posedge reset) begin
