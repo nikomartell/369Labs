b793835d570b7503f85eb12348d99c2d828bbfd6
abriltr <abriltorres@arizona.edu>
2024-10-30 09:51:54 -0700

    beyonce beyonce beyonce Top_tb.v

---
+++ Beyonce/Top_tb.v
@@ -0,0 +1,42 @@
+`timescale 1ns / 1ps
+//Percent effort:
+//Sebastian Mares - 33%
+//Abril Torres - 33%
+//Niko Martell - 33%
+
+module Top_tb;
+
+    // Inputs
+    reg clk;
+    reg rst;
+
+    // Outputs
+    wire [31:0] WriteData_sim;
+    wire [31:0] PCResult_sim;
+
+    // Instantiate the Unit Under Test (UUT)
+    Top uut (
+        .clk(clk), 
+        .rst(rst), 
+        .WriteData_sim(WriteData_sim), 
+        .PCResult_sim(PCResult_sim)
+    );
+
+    // Clock generation
+    initial begin
+        clk = 0;
+        rst = 0;
+        forever #10 clk = ~clk; // 10ns clock period
+    end
+
+    initial begin
+        // Initialize Inputs
+        rst = 1;
+
+        // Wait 100 ns for global reset to finish
+        #5;
+        rst = 0; // Release reset
+        
+    end
+
+endmodule
