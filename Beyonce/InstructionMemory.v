b93939156b51d1a52299045a40f357208d2fe475
Niko Martell <niko.martell3@gmail.com>
2024-11-01 17:42:21 -0700

    made instruction memory take instructions.mem for testing from file

--- Beyonce/InstructionMemory.v
+++ Beyonce/InstructionMemory.v
@@ -45,45 +45,10 @@ module InstructionMemory(Address, Instruction);
     reg [31:0] Memory [127:0]; //Memory to hold instructions 128 words of 32-bits memory sort of like matrix  
     integer i;
    
-   initial begin
-        Memory[0] = 32'b0; // nop
-        Memory[1] = 32'b001000_00000_00010_0000000000001110; // loop: addi $2, $0, 14
-        Memory[2] = 32'b0; // nop
-        Memory[3] = 32'b0; // nop
-        Memory[4] = 32'b0; // nop
-        Memory[5] = 32'b0; // nop
-        Memory[6] = 32'b0; // nop
-        Memory[7] = 32'b001000_00000_00011_1111_1111_1111_1110; // addi $3, $0, some large int
-        Memory[8] = 32'b0; // nop
-        Memory[9] = 32'b0; // nop
-        Memory[10] = 32'b0; // nop
-        Memory[11] = 32'b0; // nop 
-        Memory[12] = 32'b0; // nop
-        Memory[13] = 32'b000000_00010_00011_00001_00000_100111; // nor $1, $2, $3
-        Memory[14] = 32'b0; // nop
-        Memory[15] = 32'b0; // nop
-        Memory[16] = 32'b0; // nop
-        Memory[17] = 32'b0; // nop
-        Memory[18] = 32'b0; // nop
-        Memory[19] = 32'b000000_00011_00010_00100_00000_101010; // slt $4, $3, $1
-        Memory[20] = 32'b0; // nop
-        Memory[21] = 32'b0; // nop
-        Memory[22] = 32'b0; // nop
-        Memory[23] = 32'b0; // nop
-        Memory[24] = 32'b000100_00100_00101_1111111111101000; // beq $4, $5 loop 
-        Memory[25] = 32'b0; // nop
-        Memory[26] = 32'b0; // nop
-        Memory[27] = 32'b0; // nop
-        Memory[28] = 32'b0; // nop
-        Memory[29] = 32'b0; // nop
-
-
-
-
-
+    initial begin
+        $readmemb("instructions.mem", Memory);
     end
 
-    
     always @(Address) begin //Replace with text file later , for now trash values to itirate through memory    
         Instruction <= Memory[Address[8:2]];
     end 
