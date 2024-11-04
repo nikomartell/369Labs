109c65de42682359935e8f3f29ded90f8ab3c502
Niko Martell <niko.martell3@gmail.com>
2024-11-04 14:36:28 -0700

    7

--- Beyonce/Two4DigitDisplay/SevenSegment.v
+++ Beyonce/Two4DigitDisplay/SevenSegment.v
@@ -8,20 +8,20 @@ module SevenSegment(numin, segout);
         case (numin)
                 4'b0000: segout <= 7'b1000000;    //0 
                 4'b0001: segout <= 7'b1111001;    //1
-                4'b0010: segout <= 7'b0010010;    //2
+                4'b0010: segout <= 7'b0100100;    //2
                 4'b0011: segout <= 7'b0110000;    //3
                 4'b0100: segout <= 7'b0011001;    //4
                 4'b0101: segout <= 7'b0010010;    //5
                 4'b0110: segout <= 7'b0000010;    //6
                 4'b0111: segout <= 7'b1111000;    //7
                 4'b1000: segout <= 7'b0000000;    //8
-                4'b1001: segout <= 7'b0000100;    //9
+                4'b1001: segout <= 7'b0010000;    //9
                 4'b1010: segout <= 7'b0001000;    //A
-                4'b1011: segout <= 7'b1100000;    //B
-                4'b1100: segout <= 7'b0110001;    //C
-                4'b1101: segout <= 7'b1000010;    //D
-                4'b1110: segout <= 7'b0110000;    //E
-                4'b1111: segout <= 7'b0111000;    //F
+                4'b1011: segout <= 7'b0000011;    //B
+                4'b1100: segout <= 7'b1000110;    //C
+                4'b1101: segout <= 7'b0100001;    //D
+                4'b1110: segout <= 7'b0000110;    //E
+                4'b1111: segout <= 7'b0001110;    //F
                 default: segout <= 7'b1111111;  // - - -
        endcase
      end
